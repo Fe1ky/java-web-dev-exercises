b76eb8cc123872b948fff9b4c941570e5cb4a097
Fe1ky <76542408+Fe1ky@users.noreply.github.com>
2021-09-23 16:18:36 -0500

    Class 7 Exercises: Interfaces and Polymorphism completed!

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,12 +1,40 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        Comparator comparator = new FlavorComparator(); // flavors to sort
+        Comparator comparator1 = new ConeComparator();
+
+        for (Flavor object: flavors) {
+            System.out.println(object.getName());
+        }
+        System.out.println(" ");
+
+
+
+        flavors.sort(comparator); // sorts flavors
+
+        for (Flavor object: flavors) {
+            System.out.println(object.getName());
+        }
+        System.out.println(" ");
+        for (Cone object: cones) {
+            System.out.println(object.getCost());
+        }
+
+
+        cones.sort(comparator1);
+
+        System.out.println(" ");
+        for (Cone object: cones) {
+            System.out.println(object.getCost());
+        }
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
